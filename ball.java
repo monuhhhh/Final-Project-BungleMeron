4b0b11b46f68f6aa1a09699f2f1cf2ec7a19a930
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2026-01-14 08:54:14 -0500

    .

---
+++ ball.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ball here.
+ * 
+ * @author Kalkie 
+ * @version (a version number or a date)
+ */
+public class ball extends Actor{    
+        //super(600, 400, 1);
+        //addObject(new Shooter(), 100, 200);
+    }
+    
+
